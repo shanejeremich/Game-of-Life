19928ee4754a6383f3b752e4b466ec130bdaea90
Shane Jeremich <shane.jeremich@gmail.com>
2020-07-31 11:22:44 -0600

    updated about this algorithm section

--- src/App.jsx
+++ src/App.jsx
@@ -15,23 +15,24 @@ function App() {
                 <h2>About this Algorithm:</h2>
                 <p>
                     The Game of Life (sometimes known simply as Life) is an
-                    example of a cellular automaton and a zero-player game. It
-                    takes place on an infinite two-dimensional grid in which
-                    cells can be ‘on’ (alive) or ‘off’ (dead), and is defined by
-                    a set of rules that jointly determine the state of a cell
-                    given the state of its neighbours. Following specification
-                    of an initial configuration, patterns evolve over time
-                    across the grid requiring no further user input (thus
-                    ‘zero-player’). First popularized in 1970 in the Scientific
-                    American (Gardner, 1970), the Game of Life has attracted
-                    lasting appeal among both scientific and amateur
-                    communities. One reason for its appeal is that it is very
-                    simple to program, yet at the same time it appears to
-                    exemplify emergent and self-organized behaviour. Even though
-                    its (simple) rules are specified at the level of individual
-                    cells, one sees entities at coarse-grained ‘higher’ levels
-                    of description, whose behaviors are better described by
-                    rules at these higher levels.
+                    example of a cellular automaton devised by the British
+                    mathematician John Horton Conway in 1970. It takes place on
+                    an infinite two-dimensional grid in which cells can be ‘on’
+                    (alive) or ‘off’ (dead), and is defined by a set of rules
+                    that jointly determine the state of a cell given the state
+                    of its neighbours. Following specification of an initial
+                    configuration, patterns evolve over time across the grid
+                    requiring no further user input (thus ‘zero-player’). First
+                    popularized in 1970 in the Scientific American (Gardner,
+                    1970), the Game of Life has attracted lasting appeal among
+                    both scientific and amateur communities. One reason for its
+                    appeal is that it is very simple to program, yet at the same
+                    time it appears to exemplify emergent and self-organized
+                    behaviour. Even though its (simple) rules are specified at
+                    the level of individual cells, one sees entities at
+                    coarse-grained ‘higher’ levels of description, whose
+                    behaviors are better described by rules at these higher
+                    levels.
                 </p>
             </div>
         </>
