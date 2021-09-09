117b4d2e524a709b5df32949431f957ce6e963a5
Shane Jeremich <shane.jeremich@gmail.com>
2021-09-08 19:28:05 -0600

    Updates

---
+++ precache-manifest.6ea2fd222458f76c3a765102bc4e04e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a259212159fd30e319d2e7e9be8de8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a137a131226ee2866383",
+    "url": "/static/css/main.af6a30cd.chunk.css"
+  },
+  {
+    "revision": "70bfaf1b862c14962059",
+    "url": "/static/js/2.83816486.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.83816486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a137a131226ee2866383",
+    "url": "/static/js/main.33760579.chunk.js"
+  },
+  {
+    "revision": "85d367d40f4625aef104",
+    "url": "/static/js/runtime-main.1281186e.js"
+  }
+]);
